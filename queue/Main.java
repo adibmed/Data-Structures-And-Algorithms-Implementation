95b1d8eb4aae6f921fde78958500c5c9f3ff9aa6
adibemohamed <mr.eladib.mohamed@gmail.com>
2021-01-05 23:29:16 +0000

    Add Queue implementation

---
+++ queue/Main.java
@@ -0,0 +1,13 @@
+package queue;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Queue queue = new Queue();
+        queue.push(12);
+        queue.push(34);
+        queue.push(34);
+
+    }
+    
+}
---
+++ queue/Queue.java
@@ -0,0 +1,25 @@
+public class Queue {
+    public Queue() {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public void push() {
+
+    }
+
+    public void peek() {
+
+    }
+
+    public void isEmpty() {
+
+    }
+
+    public void isFull() {
+
+    }
+}
